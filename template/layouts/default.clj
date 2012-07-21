13614c3e813ec8e81a8b628d3811aa9a76e9a7a1
narkisr <narkisr@gmail.com>
2012-07-21 22:29:30 +0300

    adding rss

--- template/layouts/default.clj
+++ template/layouts/default.clj
@@ -2,6 +2,8 @@
 ; @format html5
 
 
+(defn with-root [args site] 
+ (map #(str (:root site) %) args) ) 
 
 (defn nav-bar []
   [:div {:class "navbar navbar-fixed-top"}
@@ -12,21 +14,21 @@
       [:span {:class "icon-bar"}]
       [:span {:class "icon-bar"}]
       ]
-
+     [:a {:href "/atom.xml"} 
+             [:img {:style "border-width:0;margin: 5px 0 0 0;float: right" :src "/img/rss.png"}]]
      [:a {:class "brand" :href "#"} "Narkisr.com" ]
      [:div {:class "nav-collapse"}
       [:ul {:class "nav"}
-       [:li {:class "active" } [:a {:href "/"} "Home"]]
+       [:li {:class "divider-vertical"}]
        [:li [:a {:href "/posts.html"} "Posts"]]
-       [:li [:a {:href "/projects.html"} "Projects"]]
-       ]
+       [:li [:a {:href "/projects.html"} "Projects"]]]
       ]
      ]]]
   )
 
 [:head
  [:meta {:charset (:charset site)}]
- [:meta {:name    "viewport" :content "width=device-width, initial-scale=1.0"}]
+ [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
 
  [:title
   (if (= (:title site) "home")
@@ -36,15 +38,15 @@
  [:style "body { padding-top: 80px; }"]
  [:link {:rel   "shortcut icon" :href  "/favicon.ico"}]
  [:link {:href  "/atom.xml" :rel   "alternate" :title (:title site) :type  "application/atom-xml"}]
- (css ["../css/bootstrap.min.cerulean.css" "../css/bootstrap-responsive.min.css" "../css/docs.css" ]) ]
+ (css (with-root (site :css) site)) ]
 
 [:body 
  (nav-bar)
  [:div {"class" "container"}
   contents
- [:footer {:class "footer"} 
-  [:p
-  (link (str "@" (:twitter site)) (str "http://twitter.com/" (:twitter site))) "&nbsp; 2012"]]]
- (js ["../js/prettify.js" "../js/jquery-1.7.2.min.js" "../js/bootstrap.min.js"  (:js site ())])]
+  [:footer {:class "footer"} 
+   [:p
+    (link (str "@" (:twitter site)) (str "http://twitter.com/" (:twitter site))) "&nbsp; 2012"]]]
+ (js (with-root (site :js) site))]
 
 
