c5f17365e067f75e4747fcd65b1260200d86e2d0
narkisr <narkisr@gmail.com>
2012-07-24 03:40:12 +0300

    adding clojure future post

---
+++ template/posts/2012-07-24-waiting-for.html.clj
@@ -0,0 +1,97 @@
+; @layout  post
+; @title   Waiting for your future
+; @tag     Clojure future concurrency 
+
+
+[:p "So, you've got an action that you want to run in parallel using multiple cores, in pure Java land you have the old fashioned Thread.start which takes a Runnable implementation, another option is the " (link "ExecutorService" "http://docs.oracle.com/javase/6/docs/api/java/util/concurrent/ExecutorService.html") " which uses a pool to store a collection of reusable threads (in fact its the same pool that agents use).
+     Clojure on the other hand has the " (link "future" "http://clojuredocs.org/clojure_core/clojure.core/future") " macro which takes a body of code and runs it on a seprate thread:"
+ ]
+
+#-CLJ
+(def f (future (println "hello")))
+  
+@f ; will block here until it executes 
+CLJ
+
+[:p "Futures use an unbound thread pool which is good for IO bound actions, lets assume we have a sequence (a large one at that) that serves as an input to that heavy IO operation, we want to parallelize the operation on multiple cores, the naive approach will be:"]
+
+#-CLJ
+ (defn action [i]
+    (Thread/sleep 4000)
+    (println i))
+
+(defn -main []
+  (map (future (action %)) (range 1000)))
+CLJ
+
+[:p "Running this code will result with no future run, the main thread will exit before any future gets the chance to execute (since its async), in order to wait for them to run we need to deref all futures,  "]
+
+#-CLJ
+ (defn -main []
+  (map deref (map (future (action %)) (range 1000)))) 
+CLJ
+
+[:p "This looks good and indeed when we run it we notice the following output"]
+
+#-CLJ
+0
+1 
+.. ; omitted for brevity 
+.. 
+31
+nil nil nil  ; after a delay 
+32
+..
+..
+63 ; order is random 
+CLJ
+
+[:p "So whats going on here? we a get a lot less concurrency then we'd like, what we see here is Clojure chunking in action, since the inner map is lazy we produce 32 futures in each chunk deref them and carry on to the next, we could force all futures by using doall:"]
+
+#-CLJ
+(defn -main []
+  (map deref (doall (map (future (action %)) (range 1000)))))
+CLJ
+
+[:p "This poses another problem, we issue 1000 future and hammer on, this might cause the IO target to grind into a halt (be it filesystem or remote web service), a more controled method is required.
+   
+    One option is to use Java's own ExecutorService and submit actions into that (using a bounded pool) and in fact thats exactly what" (link "pallet thread" "https://github.com/pallet/pallet-thread") " enables us to do."
+ 
+ [:div
+    "Now we want to implement two main requirements, the first generate a blocked amount of running actions, the second make sure not to block the execution of actions (by chunking for example) if threads are available to handle them: "] 
+ 
+ ]
+
+#-CLJ
+(use '[pallet.thread.executor :only (executor execute)])  
+ 
+(def pool
+  (executor {:prefix "foo" :thread-group-name "foo-grp" :pool-size 4}))
+
+(defn bound-future [f]
+ {:pre [(ifn? f)]}; saves lots of errors
+  (execute pool f))
+
+(defn wait-on [futures]
+  "Waiting on a sequence of futures, limited by a constant pool of threads"
+  (while (some identity (map (comp not future-done?) futures))
+    (Thread/sleep 1000) 
+    )) 
+
+(defn -main []
+  (wait-on (map #(bound-future (fn []  (action %))) (range 1000))))
+CLJ
+
+[:p "The print will now look like "]
+
+#-CLJ
+0
+1
+2
+3
+; a short delay for the next 4 actions to kick into action
+1
+..
+CLJ
+
+[:p "I found this method to offer a balance between the spread of work and predictable load on the target."]
